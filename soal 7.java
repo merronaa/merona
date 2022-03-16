7220261c2ddfb7595bf5acffd91d26665e0f350b
merronaa <mmehran292@gmail.com>
2022-03-17 01:40:49 +0330

    soal 8

--- soal 7.java
+++ soal 7.java
@@ -1,37 +1,62 @@
 import java.util.Scanner;
 
-class k_m_m {
-    public static void main(String[] args) {
+public class biggest {
+    public static void main(String[] args) 
+    {
+
         Scanner enter = new Scanner(System.in);
 
-        System.out.print(" INPUT YOUR NUMBER : ");
+        System.out.print("input size >>>>>");
 
-        int input = enter.nextInt();
+        int size = enter.nextInt();
 
-        System.out.println();
-        System.out.println();
-        
-        kmm(input);
-        
+        if (size > 0) 
+        {
+            int mehran[] = new int[size];
 
-    }
-    static void kmm(int input)
-    {
-        int max;
+            System.out.println(" input " + size + " elemen :");
 
-        for (int i = 2; i < input; i++) {
-            for (int j = i + 1; j < input; j++) {
-                    max = j;
-                while ((max % j != 0) || (max % i != 0)) {
-                    max++;
-                }
-                if (max != j * i) {
-                    System.out.print("( " + i + " " + j + " )  ");
-                }
+            for (int i = 0; i < size; i++) 
+            {
+                mehran[i] = enter.nextInt();
 
             }
-
+            System.out.println("-----------------------------------------------------");
+            System.out.println("( " +max(mehran, size) + " , " + negmax(mehran, size)+ " )");
         }
+        else
+        {
+            System.out.println("error :(");
+        }
+
+    }
+
+    private static int max(int array[], int size) {
+        int maxim = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[0] <= array[i]) {
+                maxim = array[i];
+            }
+        }
+        return maxim;
+    }
+
+    private static int negmax(int array[], int size) {
+        int maxim = 0;
+        int new_array[] = new int[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] < 0) {
+                new_array[count] = array[i];
+                count++;
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            if (new_array[0] <= new_array[i]) {
+                maxim = new_array[i];
+            }
+        }
+        return maxim;
 
     }
 }
